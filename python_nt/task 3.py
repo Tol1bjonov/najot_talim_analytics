a6a03f4b36434345dcaeda63b0df18c3aa0e8555
Yusufjon Tolibjonov <yusufjontolibjonov3@gmail.com>
2026-01-30 21:29:56 +0500

    list, set homeworks

--- python_nt/task 3.py
+++ python_nt/task 3.py
@@ -16,10 +16,27 @@
 # print(result)
 
 #  Puzzle 3
-nums = [3, 5, 2, 4, 1, 6]
-result = []
-for i in range(1, len(nums)):
-    if nums[i] > nums[i-1]:
-        result.append(nums[i])
-print(result)
+# nums = [3, 5, 2, 4, 1, 6]
+# result = []
+# for i in range(1, len(nums)):
+#     if nums[i] > nums[i-1]:
+#         result.append(nums[i])
+# print(result)
 
+# t= (1,2,3,4)
+# m=[]
+# for i in t:
+#     m.append(i)
+# m[2]=99
+# m2 = tuple(m)
+# print(type(m2 ))
+
+t= (10,20,30,40,50)
+# result = []
+# for i in range(len(t)):
+#     if i%2==0:
+#         result.append(t[i])
+# print(result)
+
+for i in range(0, len(t), 2):
+    print(t[i])
